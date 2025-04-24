20a28a01e4e8e18bed9baab8ed16411f2e2f3d12
Fabian-mo29 <fabian.moraarias@ucr.ac.cr>
2025-04-24 15:55:57 -0600

    Se inicializa el laboratorio 5

---
+++ Laboratorio5_C25095/backend-lab/backend-lab/Program.cs
@@ -0,0 +1,36 @@
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+var builder = WebApplication.CreateBuilder(args);
+
+// Configurar CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        policy =>
+        {
+            policy.WithOrigins("https://localhost:8080") 
+                  .AllowAnyMethod()                      
+                  .AllowAnyHeader()                      
+                  .AllowCredentials();                   
+        });
+});
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors(MyAllowSpecificOrigins);
+
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
